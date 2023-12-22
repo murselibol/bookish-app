e56d94515e463ecdd6bb29b3fe4bbb44628b4543
Mürsel Elibol <murselelibol@gmail.com>
2023-12-22 10:21:03 +0300

    feat: create project

---
+++ bookish-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bookish-app
+//
+//  Created by Mursel Elibol on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
