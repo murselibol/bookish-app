1d561dfff5c63d7814913cca15fe3ba14f845793
Mürsel Elibol <murselelibol@gmail.com>
2024-01-08 00:08:37 +0300

    feat: add static title and text fonts

--- bookish-app/Extensions/UIFont+Ext.swift
+++ bookish-app/Extensions/UIFont+Ext.swift
@@ -15,10 +15,29 @@ extension UIFont {
         case regularItalic = "Raleway-Italic"
         case medium = "Raleway-Medium"
         case semibold = "Raleway-SemiBold"
-      
     }
     
-    static func Raleway(_ type: RalewayType = .regular, size: CGFloat = UIFont.systemFontSize) -> UIFont {
+    static func Title1(_ type: RalewayType = .medium, size: CGFloat = 28.0) -> UIFont {
+        return UIFont(name: type.rawValue, size: size)!
+    }
+    
+    static func Title2(_ type: RalewayType = .medium, size: CGFloat = 22.0) -> UIFont {
+        return UIFont(name: type.rawValue, size: size)!
+    }
+    
+    static func Title3(_ type: RalewayType = .regular, size: CGFloat = 20.0) -> UIFont {
+        return UIFont(name: type.rawValue, size: size)!
+    }
+    
+    static func Text1(_ type: RalewayType = .regular, size: CGFloat = 17.0) -> UIFont {
+        return UIFont(name: type.rawValue, size: size)!
+    }
+    
+    static func Text2(_ type: RalewayType = .regular, size: CGFloat = 14.0) -> UIFont {
+        return UIFont(name: type.rawValue, size: size)!
+    }
+    
+    static func Text3(_ type: RalewayType = .regular, size: CGFloat = 12.0) -> UIFont {
         return UIFont(name: type.rawValue, size: size)!
     }
 }
