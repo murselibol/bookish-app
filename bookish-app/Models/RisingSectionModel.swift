0a7ae79d3221a330364ef99014ac5dedada12d76
Mürsel Elibol <murselelibol@gmail.com>
2024-01-13 00:24:40 +0300

    feat: add rising section model

---
+++ bookish-app/Models/RisingSectionModel.swift
@@ -0,0 +1,27 @@
+//
+//  RisingSectionModel.swift
+//  bookish-app
+//
+//  Created by Mursel Elibol on 12.01.2024.
+//
+
+import Foundation
+
+struct RisingSectionModel {
+    var rank: String
+    var title: String
+    var author: String
+}
+
+let CATEGORY_SECTION_COLORS = [
+    "#DC218C",
+    "#186450",
+    "#1E3264",
+    "#3073EC",
+    "#503750",
+    "#8C67AA",
+    "#D84101",
+    "#477D95",
+    "#767776",
+    "#513650"
+]
